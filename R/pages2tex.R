8e6933712ac325817b2b9a555e19172ac64875fd
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-15 15:07:29 +0000

    Create new functions for full year export and daily update and remove old functions.

--- R/pages2tex.R
+++ R/pages2tex.R
@@ -1,15 +1,21 @@
-library(lubridate)
 library(pages2df)
 
-source("R/source-folder.R")
-source("R/write-latex.R")
+pages2tex <- function(date, output_location) {
 
-today <- Sys.Date()
-this_month <- month(today)
-this_year <- year(today)
+  two_digit_year <- substr(lubridate::year(date), 3, 4)
+  two_digit_month <- sprintf("%02d", lubridate::month(date))
+  
+  chapters <- file.path(Sys.getenv("PAGESPATH"), "src", two_digit_year, two_digit_month) |>
+    pages_df() |>
+    create_chapter_df() |>
+    summarise_chapter_df()
+  
+  purrr::walk(dirname(file.path(output_location, chapters$path)), dir.create, recursive = TRUE, showWarnings = FALSE)
+  
+  purrr::walk2(.x = chapters$tex_chapter, .y = file.path(output_location, chapters$path), .f = readr::write_file)
 
-source_folder(year = this_year, month = this_month) |>
-  pages_df() |>
-  create_chapter_df() |>
-  summarise_chapter_df() |>
-  write_latex(output_location = Sys.getenv("OUTPATH"))
+}
+
+# Daily update invocation
+#
+# pages2tex(Sys.Date(), output_location = Sys.getenv("OUTPATH"))
--- R/source-folder.R
+++
@@ -1,5 +0,0 @@
-source_folder <- function(year = 2022, month = 9) {
-  two_digit_year <- substr(year, 3, 4)
-  two_digit_month <- sprintf("%02d", month)
-  file.path(Sys.getenv("PAGESPATH"), "src", two_digit_year, two_digit_month)
-}
