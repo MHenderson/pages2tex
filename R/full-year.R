049c4b6ae7d9a0e174187e59235dc9c70c84df68
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-16 09:23:17 +0100

    Create new functions for full year export and daily update and remove old functions. (#31)

--- R/full-year.R
+++ R/full-year.R
@@ -1,31 +1,37 @@
 library(pages2df)
 
-source("R/write-latex.R")
+full_year <- function(pagespath, output_location) {
 
-# source folder needs to modified slightly
-X <- file.path(Sys.getenv("PAGESPATH"), "src", 22) 
+  # the only change is recursive = TRUE
+  # does that work in the monthly case as well?
+  X <- tibble::tibble(
+      paths = list.files(pagespath, full.names = TRUE, recursive = TRUE)
+    ) |>
+    dplyr::mutate(
+      ymd = lubridate::ymd(gsub(".md", "", basename(paths)))
+    ) |>
+    dplyr::mutate(
+      text = purrr::map_chr(paths, readr::read_file)
+    )
+  
+  # this works (not sure where the NA month comes from)
+  chapters <- X |>
+    create_chapter_df() |>
+    create_chapters() |>
+    dplyr::filter(!is.na(month)) |>
+    dplyr::mutate(
+      path = file.path(paste0(month, ".tex"))
+    )
+  
+  purrr::walk(dirname(file.path(output_location, chapters$path)), dir.create, recursive = TRUE, showWarnings = FALSE)
+  purrr::walk2(.x = chapters$tex_chapter, .y = file.path(output_location, chapters$path), .f = readr::write_file)
 
-# the only change is recursive = TRUE
-# does that work in the monthly case as well?
-X <- tibble::tibble(
-    paths = list.files(X, full.names = TRUE, recursive = TRUE)
-  ) |>
-  dplyr::mutate(
-    ymd = lubridate::ymd(gsub(".md", "", basename(paths)))
-  ) |>
-  dplyr::mutate(
-    text = purrr::map_chr(paths, readr::read_file)
-  )
+}
 
-# this works (not sure where the NA month comes from)
-XX <- X |>
-  create_chapter_df() |>
-  create_chapters() |>
-  dplyr::filter(!is.na(month)) |>
-  dplyr::mutate(
-    path = file.path(paste0(month, ".tex"))
-  )
-
-# this is slightly different from the update script - which uses the OUTPATH environment variable (usually set to pagesbook-2024 folder)
-write_latex(XX, output_location = file.path("/Users/matthew/001_Project/Writing/Memoir/pagesbook-2022/src/chapter/"))
-
+# Full year export invocation
+#
+# output location is slightly different from pages2tex.R - which uses the OUTPATH environment variable (usually set to pagesbook-2024 folder)
+# full_year(
+#        pagespath = file.path(Sys.getenv("PAGESPATH"), "src", 22), 
+#  output_location = file.path("/Users/matthew/001_Project/Writing/Memoir/pagesbook-2022/src/chapter/")
+# )
--- R/write-latex.R
+++
@@ -1,4 +0,0 @@
-write_latex <- function(chapters, output_location = '.') {
-  purrr::walk(dirname(file.path(output_location, chapters$path)), dir.create, recursive = TRUE, showWarnings = FALSE)
-  purrr::walk2(.x = chapters$tex_chapter, .y = file.path(output_location, chapters$path), .f = readr::write_file)
-}
